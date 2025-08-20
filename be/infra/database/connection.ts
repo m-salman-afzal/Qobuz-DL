b16b62b0d8c8912f161fdc5d2dd2bc2175af2c02
m-salman-afzal <m.salmanafzal@proton.me>
2025-08-20 14:35:51 +0500

    Update database configuration to use Turso and include authentication token
    - Changed database dialect from SQLite to Turso in drizzle.config.ts.
    - Added authentication token to database connection configuration in both drizzle.config.ts and connection.ts.

--- be/infra/database/connection.ts
+++ be/infra/database/connection.ts
@@ -5,7 +5,8 @@ import { drizzle } from "drizzle-orm/libsql";
 import * as models from "./models";
 
 const client = createClient({
-    url: process.env.DB_URL || "file:./local.sqlite"
+    url: process.env.DB_URL || "file:./local.sqlite",
+    authToken: process.env.DB_AUTH_TOKEN
 });
 
 export const db = drizzle(client, {
