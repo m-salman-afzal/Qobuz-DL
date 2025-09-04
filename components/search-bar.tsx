9bddc0a52d25fe6263da7bf579b57c0ad87d2192
m-salman-afzal <m.salmanafzal@proton.me>
2025-09-04 20:26:58 +0500

    Update SearchBar component to allow flexible layout with flex-wrap

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -92,7 +92,7 @@ const SearchBar = ({
     }, [searchInput]);
 
     return (
-        <div className="flex items-center gap-2 relative">
+        <div className="flex items-center gap-2 relative flex-wrap">
             <div
                 onClick={() => inputRef.current?.focus()}
                 className="bg-background border relative sm:w-[600px] w-full tracking-wide font-semibold rounded-md flex gap-0.5 items-center py-1 px-3">
